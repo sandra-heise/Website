4fa6b12bbd1dedeaba06cb607fdd66445a50d056
sandra-heise <sandra.heise@ofinto.ch>
2026-05-28 13:36:47 +0200

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default {
-    site: 'https://sandra-heise.github.io/Website/',
-};
+export default defineConfig({
+  site: 'https://sandra-heise.github.io/Website/',
+  base: '/Website/',
+});
